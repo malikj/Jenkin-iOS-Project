d5288247bc2a9441d155e0a8f6d466d6ea583755
malikj <malik.apps@gmail.com>
2018-07-13 15:22:30 +0530

    first commit

---
+++ Demo-Jenkin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo-Jenkin
+//
+//  Created by malikj on 13/07/18.
+//  Copyright © 2018 malikj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("jenkin demo")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
