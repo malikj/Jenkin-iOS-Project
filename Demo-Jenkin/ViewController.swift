a1680a81bc9f11298b1e551d5afa1a8335821524
malikj <malik.apps@gmail.com>
2018-07-13 15:59:04 +0530

    second commit

--- Demo-Jenkin/ViewController.swift
+++ Demo-Jenkin/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("jenkin demo")
+        print("Jenkin demo")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
